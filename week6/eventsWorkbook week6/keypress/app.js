3a480223233796c1ffbff0eb89d8025ed0fd1efa
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-01-10 15:49:53 -0700

    week6

---
+++ week6/eventsWorkbook week6/keypress/app.js
@@ -0,0 +1,3 @@
+/* Your task is to display to the user the key and key code they press.
+Example of output: You've pressed the "a" key. It's key code is 65.
+Wes Bos made this https://keycode.info/ for us to use as an example. */
---
+++ week6/eventsWorkbook week6/submit/app.js
@@ -0,0 +1,3 @@
+// The "submit" event refreshes your browser by default
+// use "e.preventDefault()" in the first line of your callback function to prevent this
+// remember to pass "e" into the callback function
