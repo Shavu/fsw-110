20065bb964ff1283356cbd39dcf43638f3363907
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-01-10 15:57:03 -0700

    week6

--- week6/eventsWorkbook week6/click/app.js
+++ week6/eventsWorkbook week6/click/app.js
@@ -1,1 +1,6 @@
 // make the box disapear when the user clicks it
+var div = document.getElementsByClassName('red-box')[0];
+
+div.addEventListener('click', function (event) {
+    this.style.display = 'none';
+});
